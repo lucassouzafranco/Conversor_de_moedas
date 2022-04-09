31283515fa16fd7c2d9720a998eed127f40f8204
Lucas Souza <lucassouzafranco1@gmail.com>
2022-04-09 18:03:14 -0300

    Implementação da Awesome API
    A API antiga tinha um período de teste gratuito e depois não funcionou mais. Implementei a "Awesome API" e a função para conversão de moedas.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,22 +1,22 @@
 import './index.css';
-import React, { useEffect } from "react";
 import { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect } from "react";
 import {Icon} from "@iconify/react";
 import graphics from '../../assets/img/graphics.jpg';
 import {SiLinkedin} from "@react-icons/all-files/si/SiLinkedin";
 import {GoMarkGithub} from "@react-icons/all-files/go/GoMarkGithub";
 
-function Main () {
+const Main = () => {
 
-  const axios = require('axios');
-  const [coins, setCoins] = useState();
+  const [coins, setCoins] = useState(null);
+  const [brl, setBRL] = useState(null);
+  const [choice, setChoice] = useState('dolar');
 
   const loadData = async () => {
     try {
-        const response = await axios.get('https://v6.exchangerate-api.com/v6/1a02e137f791b3f57dcdc75a/latest/BRL');
-
+        const response = await axios.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL');
         setCoins(response.data);
-
         //console.log(docs);
       
     } catch(err) {
@@ -30,44 +30,78 @@ function Main () {
     loadData();
   }, [])
 
+  const conversion = () => {
+    if (coins) {
+      let realFloat = parseFloat(brl);
+      switch (choice) {
+
+        case 'dolar':
+          let dolarFloat = parseFloat(coins.USD.high);
+          let realToDolar = (realFloat / dolarFloat);
+          window.alert(`${realFloat} reais equilavem à ${realToDolar.toFixed(2)} dólares.`);
+          break;
+      
+        case 'euro':
+          let euroFloat = parseFloat(coins.EUR.high);
+          let realtoEuro = (realFloat / euroFloat);
+          window.alert(`${euroFloat} reais equivalem à ${realtoEuro.toFixed(2)} euros.`);
+
+        case 'libra':
+          let libraFloat = parseFloat(coins.GBP.high);
+          let realtoLibra = (realFloat / libraFloat);
+          window.alert(`${libraFloat} reais equivalem à ${realtoLibra.toFixed(2)} libras.`);
+
+        default:
+          break;
+      }
+    }
+  }
+  
+  const envio = (event) => {
+    event.preventDefault();
+  } 
+
   return(
     <>
-
-      <div className='area'>
-        <div className='imgContainer'><img src={graphics} className='imgDesign'/></div> 
-          <div className='titleInputArea'>
-              <h1>Exchange</h1>
-              <input type='number' placeholder='Ex.: R$1 (BRL):' className='field'></input>
-                <br></br><h3>Selecione a moeda 🪙</h3>
-                <form action="/action_page.php">
-                  <select name="currency" id="currency" className='designCascade'>
-                    <option value="dolar">Dólar (USD {coins.conversion_rates.USD})</option>
-                    <option value="euro">Euro (EUR {coins.conversion_rates.EUR})</option>
-                    <option value="libra">Libra (GBP {coins.conversion_rates.GBP})</option>
-                  </select><br></br><br></br>
-                  <input type="submit" value="Converter" className='submitDesign'></input>
-                </form>
-          </div>
-          <div className='footerBackground'>
-            <footer>
-              <p>This page was built using ReactJS⚛️ and an API called Exchange Rate </p>
-              <div className='containerSocial'>
-                <div className='containerIcons'>
-                  <a href='https://www.linkedin.com/in/lucas-souza-franco/'>
-                    <SiLinkedin className='icons'></SiLinkedin>
-                  </a>
-                  <a href='https://linktr.ee/lucassouzafranco'>
-                    <Icon icon="simple-icons:linktree" className='icons'/>
-                  </a>
-                  <a href='https://github.com/lucassouzafranco'>
-                    <GoMarkGithub className='icons'/>
-                  </a>
+      {coins&&(
+        <div className='area'>
+          <div className='imgContainer'><img src={graphics} className='imgDesign'/></div> 
+            <div className='titleInputArea'>
+                <h1>Exchange</h1>
+                <input type='number' placeholder='Ex.: R$1 (BRL):' className='field' onChange={(e) => setBRL(e.target.value)}></input>
+                  <br></br><h3>Selecione a moeda 🪙</h3>
+                  <form action="/action_page.php" onSubmit={envio}>
+                    <select name="currency" id="currency" className='designCascade' onChange={(e) => setChoice(e.target.value)}>
+                      <option value="dolar">Dólar (USD) {coins.USD.high}</option>
+                      <option value="euro">Euro (EUR) {coins.EUR.high}</option>
+                      <option value="libra">Libra (GBP) {coins.GBP.high}</option>
+                    </select><br></br><br></br>
+                    <input type="submit" value="Converter" className='submitDesign' onClick={() => conversion()}></input>
+                  </form>
+            </div>
+            <div className='footerBackground'>
+              <footer>
+                <div className='containerText'>
+                  <p>This page was built using ReactJS⚛️ and an API called</p>
+                  <p className='apiLink'><a href='https://docs.awesomeapi.com.br/'>Awesome API</a></p>
                 </div>
-              </div>
-            </footer>
-          </div>
-      </div>
-      
+                <div className='containerSocial'>
+                  <div className='containerIcons'>
+                    <a href='https://www.linkedin.com/in/lucas-souza-franco/'>
+                      <SiLinkedin className='icons'></SiLinkedin>
+                    </a>
+                    <a href='https://linktr.ee/lucassouzafranco'>
+                      <Icon icon="simple-icons:linktree" className='icons'/>
+                    </a>
+                    <a href='https://github.com/lucassouzafranco'>
+                      <GoMarkGithub className='icons'/>
+                    </a>
+                  </div>
+                </div>
+              </footer>
+            </div>
+        </div>
+      )}
     </>
   )
 }
